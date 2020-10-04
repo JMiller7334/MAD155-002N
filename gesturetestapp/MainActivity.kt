398411fb68641cd93fcaded34942ea237a034ba9
JMiller7334 <42582030+JMiller7334@users.noreply.github.com>
2020-10-03 23:56:55 -0500

    Gesture practice app
    Gesture practice app

---
+++ gesturetestapp/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.ebookfrenzy.gesturetestapp
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Toast
+import androidx.core.view.GestureDetectorCompat
+
+private const val DEBUG_TAG = "Gestures"
+
+class MainActivity :
+        Activity(),
+        GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener {
+
+    private lateinit var mDetector: GestureDetectorCompat
+
+    // Called when the activity is first created.
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Instantiate the gesture detector with the
+        // application context and an implementation of
+        // GestureDetector.OnGestureListener
+        mDetector = GestureDetectorCompat(this, this)
+        // Set the gesture detector as the double tap
+        // listener.
+        mDetector.setOnDoubleTapListener(this)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (mDetector.onTouchEvent(event)) {
+            true
+        } else {
+            super.onTouchEvent(event)
+        }
+    }
+
+    override fun onDown(event: MotionEvent): Boolean {
+        Log.d(DEBUG_TAG, "onDown: $event")
+        return true
+    }
+
+    override fun onFling(
+            event1: MotionEvent,
+            event2: MotionEvent,
+            velocityX: Float,
+            velocityY: Float
+    ): Boolean {
+        val msg = Toast.makeText(applicationContext, "gestureEvent: on fling", Toast.LENGTH_SHORT)
+        msg.show()
+        return true
+    }
+
+    override fun onLongPress(event: MotionEvent) {
+        val msg = Toast.makeText(applicationContext, "gestureEvent: long press", Toast.LENGTH_SHORT)
+        msg.show()
+    }
+
+    override fun onScroll(
+            event1: MotionEvent,
+            event2: MotionEvent,
+            distanceX: Float,
+            distanceY: Float
+    ): Boolean {
+        Log.d(DEBUG_TAG, "onScroll: $event1 $event2")
+        return true
+    }
+
+    override fun onShowPress(event: MotionEvent) {
+        Log.d(DEBUG_TAG, "onShowPress: $event")
+    }
+
+    override fun onSingleTapUp(event: MotionEvent): Boolean {
+        Log.d(DEBUG_TAG, "onSingleTapUp: $event")
+        return true
+    }
+
+    override fun onDoubleTap(event: MotionEvent): Boolean { //difference between double tap and double tap event?????
+       Log.d(DEBUG_TAG, "onDoubleTap: $event")
+       return true
+    }
+
+    override fun onDoubleTapEvent(event: MotionEvent): Boolean { // fires correctly through this function
+        val msg = Toast.makeText(applicationContext, "gestureEvent: Double tap", Toast.LENGTH_SHORT)
+        msg.show()
+
+        val nxtScreen = Intent(this,Main2Activity::class.java)
+        startActivity(nxtScreen)
+        return true
+    }
+
+    override fun onSingleTapConfirmed(event: MotionEvent): Boolean {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: $event")
+        return true
+    }
+}
+
